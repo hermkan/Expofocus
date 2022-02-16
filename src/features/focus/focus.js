6ec10f76c40d00f9fa7b255a3579554afe9ddfea
hermkan <hermann.kanga.dev@gmail.com>
2022-02-16 03:31:32 -0500

    focus init

--- src/features/focus/focus.js
+++ src/features/focus/focus.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import {TextInput} from 'react-native-paper'
+import { TextInput } from 'react-native-paper';
 
 export const Focus = () => {
   return (
     <View style={styles.container}>
       <Text>What would you like to focus on?</Text>
-      <TextInput/>
+      <TextInput />
     </View>
   );
 };
@@ -15,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
-    backgroundColor: "#0a192f",
-    textColor: "#fff",
+    backgroundColor: '#0a192f',
   },
 });
